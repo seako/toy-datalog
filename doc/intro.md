dae0510807b08d25c9e9eadf7a7e7713bc1f8bae
Chris Kolodin <chris.kolodin@gmail.com>
2016-10-31 22:29:05 -0700

    some bad tests and some bad docs to start us off

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to toy-datalog
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/toy_datalog/core_test.clj
@@ -0,0 +1,7 @@
+(ns toy-datalog.core-test
+  (:require [clojure.test :refer :all]
+            [toy-datalog.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
