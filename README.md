59988fb94e99ce91921686bb7687f945a0089d84
Chris Kolodin <chris.kolodin@gmail.com>
2016-10-31 22:23:07 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# toy-datalog
+
+A Clojure implementation of datalog
+
+## Usage
+
+FIXME
+
+## License
+
+This is free and unencumbered software released into the public domain. See the UNLICENSE file for more information.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject toy-datalog "0.1.0-SNAPSHOT"
+  :description "toy datalog implementation"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [instaparse "1.4.3"]])
