b0390bd94e6701a759ece14822501338a0c81a6e
Chris Kolodin <chris.kolodin@gmail.com>
2016-11-02 22:47:03 -0700

    add test program from racket docs

--- src/toy_datalog/core.clj
+++ src/toy_datalog/core.clj
@@ -4,3 +4,9 @@
 ;; Grammar from https://docs.racket-lang.org/datalog/datalog.html
 (def parse
   (i/parser (clojure.java.io/resource "datalog.bnf"), :output-format :hiccup))
+
+(def test-program
+  "edge(a, b). edge(b, c). edge(c, d). edge(d, a).
+   path(X, Y) :- edge(X, Y).
+   path(X, Y) :- edge(X, Z), path(Z, Y).
+   path(X, Y)?")
