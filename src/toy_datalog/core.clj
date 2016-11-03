ff2032a92d1b0252451b7e683e6e827a39f2f4bb
Chris Kolodin <chris.kolodin@gmail.com>
2016-11-02 23:29:14 -0700

    finally got parsing of terms right
    previously, each term of a :literal would be parsed as a :terms node with a single
    :term in it. now there is only a single :terms node for each :literal
    consisting of all of the individual :term elements. joy.

--- src/toy_datalog/core.clj
+++ src/toy_datalog/core.clj
@@ -2,6 +2,7 @@
   (require [instaparse.core :as i]))
 
 ;; Grammar from https://docs.racket-lang.org/datalog/datalog.html
+;; with slight variations to take advantages of some of Instaparse's conveniences
 (def parse
   (i/parser (clojure.java.io/resource "datalog.bnf"), :output-format :hiccup))
 
